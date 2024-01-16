b7d6ce4cc4f3d5e10837b3a7c0a566a027727f92
AlexUA_ubuntu <alex2011ua@gmail.com>
2024-01-16 14:16:42 +0100

    add sentiment

--- app/api.py
+++ app/api.py
@@ -1,10 +1,11 @@
-import pycld2 as cld2
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from jinja2 import Environment, FileSystemLoader
 from pydantic import BaseModel
-from trankit import Pipeline
+
+
+from app.servises import dell_stop_words, sentiment_analysis, detect_language, read_item
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -12,22 +13,28 @@ templates = Jinja2Templates(directory="templates")
 env = Environment(loader=FileSystemLoader("templates"))
 
 
-pipeline = Pipeline("english")
-base_of_language = ["english"]
-
-
-class FormData(BaseModel):
+class FormTextData(BaseModel):
     text: str
+
+
+class FormAnalizedTextData(BaseModel):
+    analyzed_text: list[dict]
 
 
 @app.get("/")
 def show_results(request: Request):
     """Displays a web interface to demonstrate a running application."""
-    return templates.TemplateResponse("show_results.html", {"request": request})
+    description = """
+    Barack Obama gave a fantastic speech last night. Reports indicate he will move next to New Hampshire. Barack Obama wasn't the best president.
+    Найкращим футболістом 2023 року за версією премії ФІФА The Best втретє став нападник "Інтер Маямі" та збірної Аргентини Ліонель Мессі, це новий рекорд.
+    Мессі втретє виграв нагороду від ФІФА як найкращий футболіст року.
+    """
+
+    return templates.TemplateResponse("show_results.html", {"request": request, "description":description})
 
 
 @app.post("/text_analyze")
-async def text_analyze(data: FormData) -> list[dict]:
+async def text_analyze(data: FormTextData) -> list[dict]:
     # parce list of text for piece of texts with different languages
     list_of_text = data.text.split("\n")
 
@@ -37,94 +44,24 @@ async def text_analyze(data: FormData) -> list[dict]:
 
     # lemmatization every part of text
     lemma_text: list = []
-    for pair in text_with_language_detect:
-        a: dict = await read_item((pair[0], pair[1]))
+    for pair in text_with_language_detect:  # division into sentences and add language information
+        a: dict = read_item((pair["language"], pair["text"]))
         for sentence in a["sentences"]:
-            lemma_text.append(sentence)
-    return lemma_text
+            lemma_text.append(
+                {"original_sentence": sentence["text"],
+                 "language": pair["language"],
+                 "code": pair["code"],
+                 "tokens": sentence["tokens"],
+                 }
+            )
+    # delete stop words
+    lemma_text_without_stop_words = dell_stop_words(lemma_text)
+    return lemma_text_without_stop_words
 
+@app.post("/sentiment_analyze")
+async def sentiment_analyze(data: FormAnalizedTextData) -> list[tuple]:
+    sentences: list = []
+    for sentence in data.analyzed_text:
+        sentences.append(sentiment_analysis(sentence["original_sentence"], sentence["code"]))
 
-@app.post("/lemmatization")
-async def read_item(body: tuple[str, str]) -> dict:
-    """
-        Function to lemmatize the text
-        it performs tokenization and sentence segmentation for the input document.
-        Next, it assigns a lemma to each token in the sentences.
-
-        :param body: body[0] is language, body[1] is text
-        ["english", "Hello! This is Trankit."]
-        :return:
-       {
-        "text": "Hello! This is Trankit.",
-        "sentences": [
-            {
-                "id": 1,
-                "text": "Hello!",
-                "tokens": [
-                    {
-                        "id": 1,
-                        "text": "Hello",
-                        "dspan": [0,5],
-                        "span": [0,5],
-                        "lemma": "hello"
-                    },
-                    {
-                        "id": 2,
-                        "text": "!",
-                        "dspan": [5,6],
-                        "span": [5,6],
-                        "lemma": "!"
-                    }
-                ],
-                "dspan": [0,6]
-            },
-            {
-                "id": 2,
-                "text": "This is Trankit.",
-                "tokens": [
-                    {
-                        "id": 1,
-                        "text": "This",
-                        "dspan": [7,11],
-                        "span": [0,4],
-                        "lemma": "this"
-                    },
-                   ...
-            }
-        ],
-        "lang": "english"
-    }
-    """
-    language, text = body[0], body[1]
-    if language == "unknown":
-        language = "english"
-    if language not in base_of_language:
-        pipeline.add(language)
-        base_of_language.append(language)
-    return pipeline.lemmatize(text)
-
-
-def detect_language(text: str) -> list:
-    """
-    Function to detect language and position of change language
-    :param text: Text to be analyzed
-    :return: list of language and text
-    """
-    answer = []
-    _, _, _, vectors = cld2.detect(text, returnVectors=True)
-    for vector in vectors:
-        answer.append([vector[2].lower(), text[vector[0] : vector[0] + vector[1]]])
-    return answer
-
-
-def parse_for_lemma(a: dict) -> list[str]:
-    """
-    parse big dict after trankit in little list with only lemmas
-    :param a: dict
-    :return: list
-    """
-    list_of_lemmas: list = []
-    for sentence in a["sentences"]:
-        for token in sentence["tokens"]:
-            list_of_lemmas.append(token["lemma"])
-    return list_of_lemmas
+    return sentences
---
+++ app/servises.py
@@ -0,0 +1,140 @@
+from stopwordsiso import stopwords
+from polyglot.text import Text
+from polyglot.downloader import downloader
+import pycld2 as cld2
+from trankit import Pipeline
+
+pipeline = Pipeline("english")
+base_of_language = ["english"]
+
+
+def detect_language(text: str) -> list[dict]:
+    """
+    Function to detect language and position of change language
+    :param text: Text to be analyzed
+    :return: list of language and text
+    """
+    answer = []
+    _, _, _, vectors = cld2.detect(text, returnVectors=True)
+    for vector in vectors:
+        answer.append(
+            {
+                "language": vector[2].lower(),
+                "text": text[vector[0]: vector[0] + vector[1]],
+                "code": vector[3]})
+    return answer
+
+
+def parse_for_lemma(a: dict) -> list[str]:
+    """
+    parse big dict after trankit in little list with only lemmas
+    :param a: dict
+    :return: list
+    """
+    list_of_lemmas: list = []
+    for sentence in a["sentences"]:
+        for token in sentence["tokens"]:
+            list_of_lemmas.append(token["lemma"])
+    return list_of_lemmas
+
+
+def dell_stop_words(lemma_text: list[dict]) -> list[dict]:
+    """
+    Deleting stopwords from lemmatized text.
+    :param lemma_text: lemmatized text
+    :return: lemmatized text without stopwords
+    """
+    lemma_text_without_stopwords = []
+    for sentense in lemma_text:
+        new_token = []
+        words = []
+        for token in sentense['tokens']:
+            if token['lemma'] in stopwords(sentense["code"]) or token["text"] in stopwords(sentense["code"]):
+                continue
+            new_token.append(token)
+            words.append(token['lemma'])
+        sentense_without_stopwords = ' '.join(words)
+        new_sentence = {
+            "original_sentence": sentense["original_sentence"],
+            "sentense_without_stopwords": sentense_without_stopwords,
+            "language": sentense["language"],
+            "code": sentense["code"],
+            'tokens': new_token
+        }
+        lemma_text_without_stopwords.append(new_sentence)
+
+    return lemma_text_without_stopwords
+
+
+def read_item(body: tuple[str, str]) -> dict:
+    """
+        Function to lemmatize the text
+        it performs tokenization and sentence segmentation for the input document.
+        Next, it assigns a lemma to each token in the sentences.
+
+        :param body: body[0] is language, body[1] is text
+        ["english", "Hello! This is Trankit."]
+        :return:
+       {
+        "text": "Hello! This is Trankit.",
+        "sentences": [
+            {
+                "id": 1,
+                "text": "Hello!",
+                "tokens": [
+                    {
+                        "id": 1,
+                        "text": "Hello",
+                        "dspan": [0,5],
+                        "span": [0,5],
+                        "lemma": "hello"
+                    },
+                    {
+                        "id": 2,
+                        "text": "!",
+                        "dspan": [5,6],
+                        "span": [5,6],
+                        "lemma": "!"
+                    }
+                ],
+                "dspan": [0,6]
+            },
+            {
+                "id": 2,
+                "text": "This is Trankit.",
+                "tokens": [
+                    {
+                        "id": 1,
+                        "text": "This",
+                        "dspan": [7,11],
+                        "span": [0,4],
+                        "lemma": "this"
+                    },
+                   ...
+            }
+        ],
+        "lang": "english"
+    }
+    """
+    language, text = body[0], body[1]
+    if language == "unknown":
+        language = "english"
+    if language not in base_of_language:
+        pipeline.add(language)
+        base_of_language.append(language)
+    pipeline.set_active(language)
+    return pipeline.lemmatize(text)
+
+
+def sentiment_analysis(sentense: str, code: str) -> tuple:
+    downloader.download(f"embeddings2.en")
+    if code == "un":
+        return None, None
+
+    text = Text(sentense)
+    try:
+        sentiment = sum([word.polarity for word in text.words])
+    except ValueError:
+        print(code)
+        return None, None
+    return sentense, int(sentiment)
